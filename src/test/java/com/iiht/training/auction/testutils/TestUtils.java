f7221bb17a36f93c3ecd3924fd52b11b51cfd12b
GitAutopoc <98872904+GitAutopoc@users.noreply.github.com>
2022-12-08 10:12:24 +0530

    Update TestUtils.java

--- src/test/java/com/iiht/training/auction/testutils/TestUtils.java
+++ src/test/java/com/iiht/training/auction/testutils/TestUtils.java
@@ -41,7 +41,7 @@ public class TestUtils {
 
 	public static final String GUID = "6ed39465-d6d3-4ec4-b27d-1dcb870b2992";
 	public static String customData;
-	public static final String URL = "https://yaksha-prod-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=jSTWTxtQ8kZgQ5FC0oLgoSgZG7UoU9Asnmxgp6hLLvYId/GW9ccoLw==";
+	public static final String URL = "https://yaksha-stage-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=JSssTES1yvRyHXshDwx6m405p0uSwbqnA937NaLAGX7zazwdLPC4jg==";
 	static {
 		total = 0;
 		passed = 0;
